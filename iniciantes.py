dcda32c5463a52236971434363c57688e30bf79b
Matheus Franco <matheusmfranco.mf@gmail.com>
2021-03-27 00:28:13 -0300

    Add files via upload

---
+++ iniciantes.py
@@ -0,0 +1,31 @@
+print('Bem vindo ao aprendizado de Python')
+
+# Voce também pode fazer com que o "print" seja representado na linha de baixo, se colocarmos uma quebra de linha: \n
+
+print('\nExemplo')
+
+#Caso nao queira que o console feche assim que o código for rodado, coloque um input()
+
+print('Pressione qualquer tecla para sair...')
+input()
+
+#Caso queira que o programa feche depois de um determinado tempo:
+
+import time
+
+print('Esperaremos 5 segundos...')
+time.sleep(5) #em segundos
+print('Foram passados 5 segundos!')
+input()
+
+#bibliotecas como "time" são essenciais para o desenvolvimento do código
+#Alguns exemplos de bibliotecas:
+#Numpy
+#os
+#io
+#request
+#bs4
+#colorama
+#tkinter
+#PySimpleGUI
+#logging
