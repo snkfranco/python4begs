8376b499a5f436a7b8de44a9c197b315fe255e13
Matheus Franco <matheusmfranco.mf@gmail.com>
2021-05-19 13:47:48 -0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,17 @@
+#https://github.com/Kyogin
+
+# Display on the console 
+# Mostra na tela o conteúdo dentro dos " ' ' "
+
+print("This is going to be printed")
+# Isso vai ser apresentado no console
+
+
+# x now is receiving a string: This is going to be printed
+# x agora está recebendo uma string: This is going to be printed 
+
+x = 'This is going to be printed'
+print(x)
+
+# After that prints the variable
+# Em seguida mostra no console o variável
